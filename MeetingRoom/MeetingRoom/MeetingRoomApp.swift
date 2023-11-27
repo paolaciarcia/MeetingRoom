f3c591c226b65151345c145c9c67f6b43286f24c
Paola Golombieski Ciarcia <paolaciarcia20@gmail.com>
2023-11-27 14:14:16 -0300

    Add project

---
+++ MeetingRoom/MeetingRoom/MeetingRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingRoomApp.swift
+//  MeetingRoom
+//
+//  Created by Paola Golombieski Ciarcia on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetingRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
